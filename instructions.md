3be11982cae55e23939713e7f32ddb436d6e377b
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-03-12 23:51:37 -0600

    Project 1: ended

---
+++ instructions.md
@@ -0,0 +1,9 @@
+# Steps to do it the project
+
+1. The computer choice will make using a math.ramdom method
+2. The computer choice saves in a function named **.getComputerChoise** and return their choice
+3. The user input a election between rock, paper and scissors
+4. The user input saves on the function called **.getHumanChoise**
+5. The score of computer and user will save in two variables named **humanScore** and **computerScore** on the global scope
+6. In a function called **playRound** start one round.
+7. In a function called **playGame** is for play five times.
---
+++ script.js
@@ -0,0 +1,83 @@
+
+// console.log('Hello world')
+
+// Global scope
+let computerScore = 0;
+let humanScore = 0;
+
+
+const getComputerChoice = () => {
+    // value get a random number between 1, 2 or 3
+    const value = Math.floor(Math.random() * 3 + 1);
+
+    if (value === 1) {
+        computerValue = "rock"
+    } else if (value === 2) {
+        computerValue = "paper"
+    } else {
+        computerValue = "scissors"
+    }
+
+    // console.log(value);
+    return computerValue.trim().toLowerCase();
+}
+
+// console.log(getComputerChoice());
+
+const getHumanChoice = () => {
+    humanValue = '';
+
+    while (humanValue === null || humanValue === '') {
+        humanValue = prompt("Input choice between Rock, Paper or scissors");
+        if (humanValue === null || humanValue === '')
+            alert("Value don't be null");
+    }
+
+    // console.log(humanValue);
+    return humanValue.trim().toLowerCase();
+}
+
+
+const playRound = (humanChoice, computerChoise) => {
+    const rules = {
+        rock: "scissors", // Rock beats scissors
+        scissors: "paper", // Scissors beats paper
+        paper: "rock", // Paper beats rock
+    };
+
+    if (humanChoice === computerChoise) {
+        console.log("tie!")
+    } else if (rules[humanChoice] === computerChoise) {
+        console.log("Human won!")
+        humanScore++;
+    } else if (rules[computerChoise] === humanChoice) {
+        console.log("Computer won!");
+        computerScore++;
+    }
+}
+
+const playGame = () => {
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Round: ${i}`);
+        console.log(`Human Score: ${humanScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+
+        const computerSelection = getComputerChoice();
+        console.log(`Computer selection: ${computerSelection}`);
+        const humanSelection = getHumanChoice();
+        console.log(`Human selection: ${humanSelection}`);
+        playRound(humanSelection, computerSelection);
+
+    }
+
+    console.log("Game has ended");
+    if (humanScore > computerScore) {
+        console.log("Human won the game!");
+    } else if (computerScore > humanScore) {
+        console.log("Computer won the game!");
+    } else {
+        console.log("Tie!");
+    }
+}
+
+playGame();
