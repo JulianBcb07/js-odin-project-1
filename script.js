fdee70b3f9c9045ea0b1db2d191abf77276fadbc
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-03-14 02:14:29 -0600

    status: finished -needs refactor code

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 // console.log('Hello world')
 
 // Global scope
+let currentRound = 1;
 let computerScore = 0;
 let humanScore = 0;
+let computerSelection;
+
+ended = document.createElement("h3");
+ended.classList.add("end");
+
+endedFinal = document.createElement("h3");
+endedFinal.classList.add("endFinal");
 
 
+// function that select the choice ramdomly of the computer
 const getComputerChoice = () => {
     // value get a random number between 1, 2 or 3
     const value = Math.floor(Math.random() * 3 + 1);
@@ -23,7 +32,7 @@ const getComputerChoice = () => {
 }
 
 // console.log(getComputerChoice());
-
+// function that select human choice using a prompt modal.
 const getHumanChoice = () => {
     humanValue = '';
 
@@ -37,7 +46,7 @@ const getHumanChoice = () => {
     }
 }
 
-
+// function that works the logic of one round
 const playRound = (humanChoice, computerChoise) => {
     const rules = {
         rock: "scissors", // Rock beats scissors
@@ -46,38 +55,145 @@ const playRound = (humanChoice, computerChoise) => {
     };
 
     if (humanChoice === computerChoise) {
-        console.log("tie!")
+        ended.textContent = "Tie!";
+        const container = document.querySelector(".container");
+        const content = document.querySelector(".content");
+        container.insertBefore(ended, content);
     } else if (rules[humanChoice] === computerChoise) {
-        console.log("Human won!")
+        ended.textContent = "Human won the game!";
+        const container = document.querySelector(".container");
+        const content = document.querySelector(".content");
+        container.insertBefore(ended, content);
         humanScore++;
     } else if (rules[computerChoise] === humanChoice) {
-        console.log("Computer won!");
+        ended.textContent = "Computer won the game!";
+        const container = document.querySelector(".container");
+        const content = document.querySelector(".content");
+        container.insertBefore(ended, content);
         computerScore++;
     }
 }
 
+// function that start one round
 const playGame = () => {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Round: ${i}`);
-        console.log(`Human Score: ${humanScore}`);
-        console.log(`Computer Score: ${computerScore}`);
+    round = document.querySelector(".rounds");
+    round.textContent = `Round: ${currentRound}`;
 
-        const computerSelection = getComputerChoice();
-        console.log(`Computer selection: ${computerSelection}`);
-        const humanSelection = getHumanChoice();
-        console.log(`Human selection: ${humanSelection}`);
-        playRound(humanSelection, computerSelection);
+    human = document.querySelector(".human-score");
+    human.textContent = `Human Score: ${humanScore}`;
 
-    }
+    computer = document.querySelector(".computer-score");
+    computer.textContent = `Computer Score: ${computerScore}`
 
-    console.log("Game has ended");
-    if (humanScore > computerScore) {
-        console.log("Human won the game!");
-    } else if (computerScore > humanScore) {
-        console.log("Computer won the game!");
-    } else {
-        console.log("Tie!");
-    }
+    computerSelection = getComputerChoice();
+    console.log(`Computer selection: ${computerSelection}`);
+
+    return computerSelection;
 }
 
-playGame();
+// addEventListenner
+const inputHuman = document.getElementById("input-choise");
+inputHuman.addEventListener('click', () => {
+    handleUserChoise();
+});
+
+const handleUserChoise = () => {
+    const humanSelection = getHumanChoice();
+
+    console.log(`Human selection: ${humanSelection}`);
+    humanTime = document.querySelector(".human-choise");
+    humanTime.textContent = `Human Choice: ${humanSelection}`;
+
+    computerTime = document.querySelector(".computer-choice");
+    computerTime.textContent = `Computer Choice: ${computerSelection}`;
+
+    playRound(humanSelection, computerSelection);
+
+    console.log(currentRound);
+
+    if (currentRound < 5) {
+        currentRound++;
+        human = document.querySelector(".human-score");
+        human.textContent = `Human Score: ${humanScore}`;
+        computer = document.querySelector(".computer-score");
+        computer.textContent = `Computer Score: ${computerScore}`
+        playGame();
+
+    } else if (computerScore > humanScore) {
+        human = document.querySelector(".human-score");
+        human.textContent = `Human Score: ${humanScore}`;
+        computer = document.querySelector(".computer-score");
+        computer.textContent = `Computer Score: ${computerScore}`
+        endedFinal.textContent = "Game Ended, Computer won the game!";
+        const container = document.querySelector(".container");
+        const content = document.querySelector(".content");
+        container.insertBefore(endedFinal, content);
+        console.log("Computer won the game!");
+        inputHuman.disabled = true;
+        play.disabled = true;
+        restart.disabled = false;
+    } else if (humanScore > computerScore) {
+        human = document.querySelector(".human-score");
+        human.textContent = `Human Score: ${humanScore}`;
+        computer = document.querySelector(".computer-score");
+        computer.textContent = `Computer Score: ${computerScore}`
+        endedFinal.textContent = "Game Ended, Human won the game!";
+        const container = document.querySelector(".container");
+        const content = document.querySelector(".content");
+        container.insertBefore(endedFinal, content);
+        inputHuman.disabled = true;
+        play.disabled = true;
+        restart.disabled = false;
+    } else {
+        endedFinal.textContent = "Game Ended, Tie!";
+        const container = document.querySelector(".container");
+        const content = document.querySelector(".content");
+        container.insertBefore(endedFinal, content);
+        inputHuman.disabled = true;
+        play.disabled = true;
+        restart.disabled = false;
+    }
+
+}
+
+const play = document.getElementById("play-game");
+play.addEventListener('click', () => {
+    playGame();
+    play.disabled = true;
+    const inputHuman = document.getElementById("input-choise");
+    inputHuman.disabled = false;
+    const restart = document.getElementById("game-restart")
+    restart.disabled = true;
+});
+
+const restart = document.getElementById("game-restart");
+restart.addEventListener("click", () => {
+    round = document.querySelector(".rounds");
+    round.textContent = ``;
+
+    ended = document.querySelector(".end");
+    ended.textContent = "";
+
+    endedFinal = document.querySelector(".endFinal");
+    endedFinal.textContent = "";
+
+    human = document.querySelector(".human-score");
+    human.textContent = ``;
+
+    computer = document.querySelector(".computer-score");
+    computer.textContent = ``
+
+    humanTime = document.querySelector(".human-choise");
+    humanTime.textContent = ``;
+
+    computerTime = document.querySelector(".computer-choice");
+    computerTime.textContent = ``;
+
+    computerScore = 0;
+    humanScore = 0;
+    currentRound = 1;
+
+    inputHuman.disabled = true;
+    restart.disabled = true;
+    play.disabled = false;
+});
